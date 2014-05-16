afb2ad01c2f31f91c97287d104ed702909c9e39e
Aaron Campbell <aaronthefff@gmail.com>
2014-05-16 06:31:25 -0400

    Added a bunch of items

--- js/game.js
+++ js/game.js
@@ -3,8 +3,14 @@ var Game = function() {
   this.monies = 0;
   this.moneyRatePerClick = 1;
   this.moneyRatePerSecond = 0;
-  this.mpcUpgrades = ['Hire Employee', 'Buy Robots'];
-  this.mpsUpgrades = ['Farm', 'Factory'];
+  
+    this.mpcNames = ['Time Management Course', 'Better Equipment', 'Assistant'];
+    this.mpsNames = ['Taco Truck', 'Farm', 'Factory'];
+    this.mpcPrices = [10, 1000, 100000];
+    this.mpsPrices = [100, 5000, 250000];
+    this.mpcCount = [0, 0, 0];
+    this.mpsCount = [0, 0, 0];
+  
   // Cache a bound onFrame since we need it each frame.
     this.onFrame = this.onFrame.bind(this);
   
@@ -20,7 +26,7 @@ Game.prototype.onFrame = function() {
   
   this.monies += this.moneyRatePerSecond*delta;
   //document.title = ""+Math.round(this.monies);
-  console.log(this.currentMoney());
+  //console.log(this.currentMoney());
   
   $("span#currentMoney").html(this.currentMoney());
   
@@ -30,9 +36,64 @@ Game.prototype.onFrame = function() {
 Game.prototype.currentMoney = function() {
     return Math.round(this.monies);
 };
-Game.prototype.moneyClick = function(clicks) {
-    this.monies += this.moneyRatePerClick * clicks;
-    console.log(this.moneyRatePerClick * clicks);
+Game.prototype.moneyClick = function(clicks) { this.monies += this.moneyRatePerClick * clicks; };
+Game.prototype.buyUpgrade = function(type, tier) {
+    var price;
+    if (type == 'mpc')
+    {
+        // Check wallet
+        price = this.mpcPrices[tier];
+        if (this.monies >= price)
+        {
+            // Purchase item
+            this.monies -= price;
+            this.mpcCount[tier]++;
+            this.mpcUpdate();
+            
+            // Reset price
+            this.mpcPrices[tier] = tier * 1000 + Math.floor(10 * Math.pow(1.1, this.mpcCount[tier]));
+            price = this.mpcPrices[tier];
+            $("span#mpcUpgradePrice" + tier).html(price);
+        }
+    }
+    else if (type == 'mps')
+    {
+        // Check wallet
+        price = this.mpsPrices[tier];
+        if (this.monies >= price)
+        {
+            // Purchase item
+            this.monies -= price;
+            this.mpsCount[tier]++;
+            this.mpsUpdate();
+            
+            // Reset price
+            this.mpsPrices[tier] = 100 + tier * 5000 + Math.floor(10 * Math.pow(1.2, this.mpsCount[tier]));
+            price = this.mpsPrices[tier];
+            
+            $("span#mpsUpgradePrice" + tier).html(price);
+        }
+    }
+};
+Game.prototype.mpcUpdate = function() {
+    var mpc = 1;
+    var multi = 1;
+    for (var i = 0; i < this.mpcNames.length; i++)
+    {
+        mpc += this.mpcCount[i] * multi;
+        multi *= 10;
+    }
+    this.moneyRatePerClick = mpc;
+};
+Game.prototype.mpsUpdate = function() {
+    var mps = 0;
+    var multi = 1;
+    for (var i = 0; i < this.mpsNames.length; i++)
+    {
+        mps += this.mpsCount[i] * multi;
+        multi *= 10;
+    }
+    this.moneyRatePerSecond = mps;
 };
 
 /**
@@ -51,12 +112,20 @@ Game.prototype.moneyClick = function(clicks) {
 
 var gameInstance = new Game();
 
-//Button Names
-for (var i = 0; i < 2; i++)
-{
-    $("button#mpcUpgrade" + i).html("Buy " + gameInstance.mpcUpgrades[i]);
-    $("button#mpsUpgrade" + i).html("Buy " + gameInstance.mpsUpgrades[i]);
-}
+var buyUpgrades = (function(obj) { gameInstance.buyUpgrade(obj.data.type, obj.data.tier); });
 
+//Once per Upgrade
+for (var i = 0; i < gameInstance.mpcNames.length; i++)
+{
+    //Button Names
+    $("span#mpcUpgradeLabel" + i).html(gameInstance.mpcNames[i]);
+    $("span#mpsUpgradeLabel" + i).html(gameInstance.mpsNames[i]);
+    $("span#mpcUpgradePrice" + i).html(gameInstance.mpcPrices[i]);
+    $("span#mpsUpgradePrice" + i).html(gameInstance.mpsPrices[i]);
+    
+    //Event Binding
+    $("button#mpcUpgrade" + i).click({type: 'mpc', tier: i}, buyUpgrades);
+    $("button#mpsUpgrade" + i).click({type: 'mps', tier: i}, buyUpgrades);
+}
 // Events
 $("button#clickMoney").click(function() { gameInstance.moneyClick(1) });
